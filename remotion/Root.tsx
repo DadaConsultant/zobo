09d5e62ea30d26d972891915f231ba7cbef90a5f
Ade Dada <ade.s.dada@gmail.com>
2026-05-05 20:49:59 +0100

    marketing-video updates on landing page

--- remotion/Root.tsx
+++ remotion/Root.tsx
@@ -5,8 +5,14 @@ import {
   MARKETING_FPS,
   MARKETING_HEIGHT,
   MARKETING_WIDTH,
+  X_POST_COMPOSITION_ID,
+  X_POST_DURATION_FRAMES,
+  X_POST_FPS,
+  X_POST_HEIGHT,
+  X_POST_WIDTH,
 } from "./constants";
 import { ZoboMarketingVideo } from "./compositions/ZoboMarketingVideo";
+import { ZoboXExplainerVideo } from "./compositions/ZoboXExplainerVideo";
 
 export function RemotionRoot() {
   return (
@@ -18,6 +24,14 @@ export function RemotionRoot() {
         fps={MARKETING_FPS}
         width={MARKETING_WIDTH}
         height={MARKETING_HEIGHT}
+      />
+      <Composition
+        id={X_POST_COMPOSITION_ID}
+        component={ZoboXExplainerVideo}
+        durationInFrames={X_POST_DURATION_FRAMES}
+        fps={X_POST_FPS}
+        width={X_POST_WIDTH}
+        height={X_POST_HEIGHT}
       />
     </>
   );
